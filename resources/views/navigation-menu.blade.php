1f123004cf0f0075603d5766e266b095c7718bdd
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-23 01:38:47 +0300

    Fund Account

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -102,6 +102,10 @@
                                 {{ __('Profile') }}
                             </x-dropdown-link>
 
+                            <x-dropdown-link href="{{ route('profile.show') }}">
+                                {{ __('Fund Account') }}
+                            </x-dropdown-link>
+
                             @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                                 <x-dropdown-link href="{{ route('api-tokens.index') }}">
                                     {{ __('API Tokens') }}
