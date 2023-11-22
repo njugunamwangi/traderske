07b937574b79ed4670346ba9318e6fb1d11b377d
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-23 01:39:51 +0300

    History

---
+++ resources/views/history.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                History
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view('/history', 'history')->name('history');
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
