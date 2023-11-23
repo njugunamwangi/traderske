ac3661b563aadea1c59055182041b501ffef112b
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-24 00:09:26 +0300

    Offer

---
+++ app/Livewire/Offer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Offer extends Component
+{
+    public function render()
+    {
+        return view('livewire.offer');
+    }
+}
---
+++ resources/views/livewire/offer.blade.php
@@ -0,0 +1,26 @@
+<div>
+
+    <x-slot name="header">
+        <div class="flex justify-between">
+
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Offers') }}
+            </h2>
+            <div>
+                <a href="{{ route('create-offer') }}" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Create Offer
+                </a>
+            </div>
+
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                Offers
+            </div>
+        </div>
+    </div>
+
+</div>
