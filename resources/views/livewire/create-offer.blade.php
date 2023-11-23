e657cb7bac6f649925f5830f91a215a48ec435d7
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-24 00:09:14 +0300

    Create offer

---
+++ resources/views/livewire/create-offer.blade.php
@@ -0,0 +1,19 @@
+<div>
+
+    <x-slot name="header">
+
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Create Offer') }}
+            </h2>
+
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                Offers
+            </div>
+        </div>
+    </div>
+
+</div>
